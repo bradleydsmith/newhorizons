9ea75e37b806441bea22f8abf0da5205fb97d2ee
Bradley Smith <s3318889@student.rmit.edu.au>
2018-08-12 03:12:59 +1000

    Custom isAdmin Blade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -15,6 +17,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        Blade::if('isAdmin', function () {
+			return (Auth::check() && Auth::user()->isAdmin());
+		});
     }
 
     /**
