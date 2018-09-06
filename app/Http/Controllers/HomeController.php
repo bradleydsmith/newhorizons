c6ca80bb025f618f1fa754e309ff945401a02e15
Bradley Smith <s3318889@student.rmit.edu.au>
2018-09-06 15:02:12 +1000

    Temporarily display booking ids for user bookings made

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use View;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        //return view('home');
+        $user = Auth::user();
+        $bookings = $user->bookings()->get();
+        return View::make('home')->with('bookings', $bookings);
     }
 }
