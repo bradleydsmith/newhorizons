243c0140691f61380e6fe061085e5be0cd13573f
Bradley Smith <s3318889@student.rmit.edu.au>
2018-10-11 10:02:54 +1100

    Fix viewtrip maps - use a static leaflet map instead

--- app/Http/Controllers/ViewTripController.php
+++ app/Http/Controllers/ViewTripController.php
@@ -30,16 +30,7 @@ class ViewTripController extends Controller
         $bookingId = $request->input("bookingId");
         $booking = Booking::find($bookingId);
         
-        $trip = json_decode($booking->trip);
-        $tripUrl = "/api/staticmap?format=jpg&size=400x400&markers=color:green|size:small|";
-        $tripUrl = $tripUrl . $trip[0][0] . "," . $trip[0][1];
-        $tripUrl = $tripUrl . "&markers2=color:red|size:small|";
-        $tripUrl = $tripUrl . $trip[count($trip)-1][0] . "," . $trip[count($trip)-1][1];
-        $tripUrl = $tripUrl . "&path=";
-        for ($i = 0; $i < count($trip)-1; $i++) {
-			$tripUrl = $tripUrl . $trip[$i][0] . "," . $trip[$i][1] . "|";
-		}
-		$tripUrl = $tripUrl . $trip[count($trip)-1][0] . "," . $trip[count($trip)-1][1];
-        return View::make('viewtrip')->with('tripUrl', $tripUrl);
+        $tripJson = $booking->trip;
+        return View::make('viewtrip')->with('tripJson', $tripJson);
     }
 }
--- resources/views/viewtrip.blade.php
+++ resources/views/viewtrip.blade.php
@@ -4,6 +4,16 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
+   integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
+   crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
+   integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
+   crossorigin=""></script>
+   <link rel="stylesheet" href="css/leaflet-beautify-marker-icon.css"/>
+   <script src="js/leaflet-beautify-marker-icon.js"></script>
+   <script src="js/jquery.documentsize.js"></script>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -16,12 +26,57 @@
                             {{ session('status') }}
                         </div>
                     @endif
-                    @if(!empty($tripUrl))
-						<img style="width: 80%" src="{{ $tripUrl }}">
+                    @if(!empty($tripJson))
+						<div id="map" style="height:70%"></div>
                     @endif
                 </div>
             </div>
         </div>
     </div>
 </div>
+<script>
+	window.trip = {!! $tripJson !!};
+	console.log(trip);
+	window.map = L.map('map');
+	L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map);
+	map.attributionControl.remove();
+	map.zoomControl.remove();
+	map.dragging.disable();
+    map.touchZoom.disable();
+    map.doubleClickZoom.disable();
+    map.scrollWheelZoom.disable();
+	L.polyline(trip).addTo(map);
+	startLat = trip[0][0];
+	startLng = trip[0][1];
+	endLat = trip[trip.length-1][0];
+	endLng = trip[trip.length-1][1];
+	var options = {
+          icon: 'car',
+          iconShape: 'marker',
+          borderColor: '#b3334f',
+          textColor: '#b3334f'
+    };
+    L.marker([startLat, startLng], {
+          icon: L.BeautifyIcon.icon(options),
+          interactive: false
+        }).addTo(map);
+    var options = {
+          icon: 'car',
+          iconShape: 'marker',
+          borderColor: 'green',
+          textColor: 'green'
+    };
+    L.marker([endLat, endLng], {
+          icon: L.BeautifyIcon.icon(options),
+          interactive: false
+        }).addTo(map);
+	map.fitBounds(trip);
+	
+	//window.addEventListener("resize", function(){resize()}, true)
+	
+	function resize() {
+		map.invalidateSize();
+		map.fitBounds(trip);
+	}
+</script>
 @endsection
